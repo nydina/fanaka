6bd5775a066b4b2d07b457765bcdc8b4913e46c7
Razafindratsira Ny Dina <nydina.razafindratsira@legrand.com>
2024-02-12 16:49:33 +0100

    Enable user interaction for left and right arrow

---
+++ Fanaka/Furniture.swift
@@ -0,0 +1,26 @@
+import Foundation
+
+struct Furniture {
+    let image: String
+    let description: String
+}
+
+
+let furnitures = [
+    Furniture(
+        image: "1",
+        description: "Sofa vert en velour"
+    ),
+    Furniture(
+        image: "2",
+        description: "Ce luminaire est tout simplement parfait pour sublimer votre table à manger ou un salon"
+    ),
+    Furniture(
+        image: "3",
+        description: "Chaise blanche de style scandinave"
+    ),
+    Furniture(
+        image: "4",
+        description: " Fauteuil élégant accompagné de son repose pieds jaune orangé"
+    )
+]
--- Fanaka/ViewController.swift
+++ Fanaka/ViewController.swift
@@ -1,19 +1,47 @@
-//
-//  ViewController.swift
-//  Fanaka
-//
-//  Created by Dina RAZAFINDRATSIRA on 12/02/2024.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var furnitureImageView: UIImageView!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var leftArrowImageView: UIImageView!
+    @IBOutlet weak var rightArrowImageView: UIImageView!
+
+    var currentFurnitureIndex = 0
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateUI()
+
+        // Enable user interaction for the image views
+        leftArrowImageView.isUserInteractionEnabled = true
+        rightArrowImageView.isUserInteractionEnabled = true
+
+        // Add tap gesture recognizers to the image views
+        let leftTapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(leftArrowImageViewTapped))
+        leftArrowImageView.addGestureRecognizer(leftTapGestureRecognizer)
+
+        let rightTapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(rightArrowImageViewTapped))
+        rightArrowImageView.addGestureRecognizer(rightTapGestureRecognizer)
     }
 
+    func updateUI() {
+        let currentFurniture = furnitures[currentFurnitureIndex]
+        furnitureImageView.image = UIImage(named: currentFurniture.image)
+        descriptionLabel.text = currentFurniture.description
+    }
 
+    @objc func leftArrowImageViewTapped() {
+        if currentFurnitureIndex > 0 {
+            currentFurnitureIndex -= 1
+            updateUI()
+        }
+    }
+
+    @objc func rightArrowImageViewTapped() {
+        if currentFurnitureIndex < furnitures.count - 1 {
+            currentFurnitureIndex += 1
+            updateUI()
+        }
+    }
 }
-
